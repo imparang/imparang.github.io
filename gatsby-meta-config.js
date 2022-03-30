4c6f80e0bd9fbfe212d6741be0ee34b0382a6270
imparang <jeongjaeyn@gmail.com>
2022-03-30 17:33:13 +0900

    a

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,28 +1,25 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `EKKo.io`,
+  description: `Blog posted about EKKo's Dev Note`,
+  author: `EKKo`,
+  introduction: `[EKKo's Dev Note] 개발자 에코의 성장 일지`,
+  siteUrl: `https://imparang.github.io`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
-    instagram: ``, // Your Instagram account
+    github: `https://github.com/imparang`, // Your GitHub account
+    linkedin: `https://www.linkedin.com/in/%EC%9E%AC%EC%9C%A4-%EC%A0%95-47710922b/`, // Your LinkedIn account
+    instagram: `https://www.instagram.com/jeluzoa/`, // Your Instagram account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'EKKo', // Your disqus-short-name. check disqus.com.
+    utterances: 'imparang/imparang.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'ekko',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
